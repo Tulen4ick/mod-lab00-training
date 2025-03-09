c2f0240c9e09043347ff7c5de8518bf0d3baee3a
Tulen4ick <zaharsazanov2015@gmail.com>
2025-03-09 13:45:31 +0300

    update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,6 +1,12 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
-int64_t power(int64_t x, uint16_t n) {
-    return 0;
+int64_t power(int64_t x, uint16_t n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; ++i)
+    {
+        result *= x;
+    }
+    return result;
 }
